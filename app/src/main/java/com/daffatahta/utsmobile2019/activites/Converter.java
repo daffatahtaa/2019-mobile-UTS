6ac6c0613659ef0a8ee2de03ed6b0116a2787c7e
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-10-10 23:25:46 +0700

    added button converter timer.

---
+++ app/src/main/java/com/daffatahta/utsmobile2019/activites/Converter.java
@@ -0,0 +1,16 @@
+package com.daffatahta.utsmobile2019.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.daffatahta.utsmobile2019.R;
+
+public class Converter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_converter);
+    }
+}
--- app/src/main/java/com/daffatahta/utsmobile2019/activites/TimerActivity.java
+++ app/src/main/java/com/daffatahta/utsmobile2019/activites/TimerActivity.java
@@ -1,14 +1,16 @@
-package com.daffatahta.uts;
+package com.daffatahta.utsmobile2019.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.daffatahta.utsmobile2019.R;
+
+public class TimerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timer);
     }
 }
