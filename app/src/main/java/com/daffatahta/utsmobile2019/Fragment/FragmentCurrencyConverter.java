03df95449fbe8442b33e651920cec2a8da40aab1
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2019-10-13 12:55:14 +0700

    design updated, fragment added, timer done

--- app/src/main/java/com/daffatahta/utsmobile2019/Fragment/FragmentCurrencyConverter.java
+++ app/src/main/java/com/daffatahta/utsmobile2019/Fragment/FragmentCurrencyConverter.java
@@ -27,10 +27,6 @@ public class FragmentCurrencyConverter extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public FragmentCurrencyConverter() {
         // Required empty public constructor
@@ -57,10 +53,7 @@ public class FragmentCurrencyConverter extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -70,42 +63,4 @@ public class FragmentCurrencyConverter extends Fragment {
         return inflater.inflate(R.layout.fragment_fragment_currency_converter, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
--- app/src/main/java/com/daffatahta/utsmobile2019/activites/Converter.java
+++ app/src/main/java/com/daffatahta/utsmobile2019/activites/Converter.java
@@ -1,9 +1,15 @@
 package com.daffatahta.utsmobile2019.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.daffatahta.utsmobile2019.Fragment.FragmentCurrencyConverter;
+import com.daffatahta.utsmobile2019.Fragment.FragmentTemperature;
 import com.daffatahta.utsmobile2019.R;
 
 public class Converter extends AppCompatActivity {
@@ -12,5 +18,30 @@ public class Converter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_converter);
+    }
+
+    public void handleCurrencyConverter(View view) {
+        final Button btn;
+        btn = findViewById(R.id.buttonCurrency);
+        btn.setVisibility(View.INVISIBLE);
+
+        final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.currencyConversionPlaceholder, new FragmentCurrencyConverter());
+        ft.commit();
+
+    }
+
+    public void handleTemperatureConversion(View view) {
+        //Intent i = new Intent(this, FragmentTemperature.class);
+        //startActivity(i);
+        final Button btn;
+        btn = findViewById(R.id.buttonTemperature);
+        //hide button
+        btn.setVisibility(View.INVISIBLE);
+        //call fragment
+        final FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragmentTemperaturePlaceHolder, new FragmentTemperature());
+        ft.commit();
+
     }
 }
